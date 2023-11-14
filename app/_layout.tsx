304944aef55bf4a0771b4541433ad36c562f3be1
imsteff <stphnencomienda30@gmail.com>
2023-11-14 08:18:13 +0800

    Updated and modified the CustomHeader

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,6 +13,7 @@ import Toast from 'react-native-toast-message';
 import { Searchbar } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import CustomHeader from '../constants/CustomHeader'; 
 
 
 import Sample from './sample';
@@ -58,95 +59,6 @@ export default function RootLayout() {
   );
 }
 
-// HEADER (TOP APP BAR)
-
-function CustomHeader() {
-  const [searchQuery, setSearchQuery] = useState('');
-
-  return (
-    <View style={styles.headerContainer}>
-      <Image
-        source={require("../assets/images/LOGO-ONLY-FINAL.png")}
-        style={styles.logo}
-      />
-      <View style={styles.searchSection}>
-        <Ionicons name="search-outline" size={20} color="gray" style={styles.searchIcon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Search"
-          onChangeText={text => setSearchQuery(text)}
-          value={searchQuery}
-        />
-        {searchQuery ? (
-          <Ionicons
-            name="close-outline"
-            size={20}
-            color="gray"
-            style={styles.clearIcon}
-            onPress={() => setSearchQuery('')}
-          />
-        ) : null}
-      </View>
-      <Ionicons name="notifications-outline" size={24} color="#2E603A" style={styles.notificationIcon} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  headerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-    elevation: 4,
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    shadowOffset: { width: 0, height: 2 },
-    shadowColor: '#000',
-    height: 100,
-    paddingHorizontal: 10,
-  },
-  logo: {
-    width: 30, 
-    height: 30,
-    resizeMode: 'contain',
-    marginRight: 10,
-    marginTop: 15,
-  },
-  searchSection: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#F9F9F8',
-    borderRadius: 20,
-    paddingHorizontal: 6,
-    marginTop: 20,
-    height: 40,
-    flex: 1, 
-  },
-  searchIcon: {
-    padding: 5,
-  },
-  input: {
-    flex: 1,
-    paddingTop: 10,
-    paddingRight: 10,
-    paddingBottom: 10,
-    paddingLeft: 0,
-    backgroundColor: '#F9F9F8',
-    color: '#2D2C2D',
-    borderRadius: 20,
-  },
-  clearIcon: {
-    padding: 5,
-  },
-    notificationIcon: {
-    padding: 5,
-    fontSize:30,
-    marginTop:18,
-  },
-});
-
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
@@ -156,10 +68,13 @@ function RootLayoutNav() {
           <GestureHandlerRootView style={{ flex: 1 }}>
             <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
               <Stack>
-                <Stack.Screen name="index" 
-            options={{
-                header: () => <CustomHeader />, 
-              }} />
+                <Stack.Screen 
+                  name="index" 
+                  options={{
+                      headerShown: false,
+                      header: () => <CustomHeader />
+                      }}
+            />
                 <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
                 <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
                 <Stack.Screen name="paper" />
@@ -175,3 +90,4 @@ function RootLayoutNav() {
   
   );
 }
+
